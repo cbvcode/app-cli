3126c59dc652f66494602a64aaa2ccfe04467688
cbvdev <cbvcode@gmail.com>
2025-03-16 01:52:26 +0200

    start

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const { execSync } = require('child_process');
 program
   .command('init')
   .option('-t, --template <template>', 'Template to use')
+  .option('-p, --password <password>', 'Password for use')
   .action((options) => {
     const template = options.template;
+    const pass = options.password;
+
+    if (!pass || pass !== 'pass123') {
+      console.error('Authentication error: Invalid password or password not specified');
+      return;
+    }
 
     if (template === 'apps') {
       // execSync('npx create-next-app@latest my-app --example @your-username/your-package-name', { stdio: 'inherit' });
