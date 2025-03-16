8229eaf89b8ef9545d82438b01161196362b812f
cbvdev <cbvcode@gmail.com>
2025-03-16 02:18:09 +0200

    start

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 #!/usr/bin/env node
-const { program } = require('commander');
-const { execSync } = require('child_process');
+import * as prompts from '@clack/prompts';
+import { execSync } from 'child_process';
 
-program
-  .command('init')
-  .option('-t, --template <template>', 'Template to use')
-  .option('-p, --password <password>', 'Password for use')
-  .action((options) => {
-    const template = options.template;
-    const pass = options.password;
+async function main() {
+  const password = await prompts.password({
+    message: 'Enter password'
+  });
+  if (!password || password !== 'pass123') {
+    console.error('Authentication error: Incorrect password or password not specified');
+    return;
+  }
 
-    if (!pass || pass !== 'pass123') {
-      console.error('Authentication error: Invalid password or password not specified');
-      return;
-    }
-
-    if (template === 'apps') {
-      // execSync('npx create-next-app@latest my-app --example @your-username/your-package-name', { stdio: 'inherit' });
-      execSync('npx create-next-app@latest app', { stdio: 'inherit' });
-    } else {
-      console.error('Invalid template');
-    }
+  const response = await prompts.group({
+    command: () => prompts.select({
+      message: 'Choose the command',
+      options: [
+        { value: 'init', label: 'Project initialization' }
+      ]
+    }),
+    template: ({ command }) => command === 'init' ? prompts.select({
+        message: 'Choose a project template',
+        options: [
+          { value: 'next-app', label: 'Next.js app' },
+        ]
+      }) : null,
   });
 
-program.parse(process.argv);
+  if (response.command === 'init') {
+    switch (response.template) {
+      case 'next-app':
+        execSync('npx create-next-app@latest app', { stdio: 'inherit' });
+        break;
+
+      default:
+        console.error('The template is not supported');
+    }
+  }
+}
+
+main().catch(console.error);
